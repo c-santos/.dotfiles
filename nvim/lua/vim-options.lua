059f356bdd6c5be6161f26e0c9d6a103ae9891ed
jcsantos <jcsantos@ubx.ph>
2024-07-04 13:03:22 +0800

    chore: update vim updatetime

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -19,7 +19,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 100
 
 vim.opt.colorcolumn = "80"
 vim.opt.mouse = 'a'
