d25068fcf45c15a6cef7c331619e2c65963c56e0
c-santos <jcms121899@gmail.com>
2024-07-11 15:20:16 +0800

    added lin numbers to telescope preview

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,7 +1,15 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'TelescopePreviewerLoaded',
+    desc = 'Show line number in telescope preview',
+    callback = function(args)
+        vim.wo.number = true
+    end,
 })
