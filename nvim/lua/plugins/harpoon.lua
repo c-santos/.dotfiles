92931d0fe6da3b64d325071215267faca20a908d
c-santos <jcms121899@gmail.com>
2024-07-23 22:33:11 +0800

    chore(nvim): updated harpoon remaps

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -11,10 +11,7 @@ return {
         vim.keymap.set("n", "<leader>a", mark.add_file)
         vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 
-        vim.keymap.set("n", "<C-i>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-o>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-j>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-k>", function() ui.nav_file(4) end)
-
+        vim.keymap.set("n", "<C-i>", function() ui.nav_next() end)
+        vim.keymap.set("n", "<C-o>", function() ui.nav_prev() end)
     end
 }
