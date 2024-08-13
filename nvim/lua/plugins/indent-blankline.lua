b183ae08b68804da417ed59a80eaf4b6859eea0d
c-santos <jcms121899@gmail.com>
2024-08-13 09:29:14 +0800

    chore(nvim): made indent-blankline thinner

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,3 +1,13 @@
 -- adds indent line ( line showing scope )
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
-
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+        require('ibl').setup({
+            indent = {
+                char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+            },
+        })
+    end,
+    opts = {},
+}
