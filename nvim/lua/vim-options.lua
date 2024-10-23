2cc2e86ba6baa7caf9c04b5945c1484909cdcd33
c-santos <jcms121899@gmail.com>
2024-10-23 15:09:33 +0800

    nvim: ignore node_modules in vim search

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,6 +5,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.wildignore:append({"*/node_modules/*"})
 
 -- buffer ui settings
 vim.opt.title = true
