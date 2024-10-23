74c0810b8d2404b2c1fe488dc92a7d1efc1a9a38
c-santos <jcms121899@gmail.com>
2024-10-23 14:50:12 +0800

    nvim: updated vim-options

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,39 +1,52 @@
-vim.cmd("set relativenumber")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set linebreak")
-vim.cmd("set clipboard=unnamedplus")
-
-vim.opt.nu = true
-vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro' -- netrw line num
-vim.g.have_nerd_font = true
-vim.opt.cursorline = true
-vim.opt.showmode = false
-vim.opt.incsearch = true
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+-- general vim settings
 vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
-vim.o.guicursor=""
-
-vim.opt.updatetime = 100
-
-vim.opt.colorcolumn = "80"
-vim.opt.mouse = 'a'
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+-- buffer ui settings
+vim.opt.title = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.showmode = false
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.g.have_nerd_font = true
+vim.o.guicursor = "" -- block cursor on insert mode
+vim.opt.colorcolumn = "80"
+
+-- buffer behavior settings
+
+-- tabs and indents
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = false
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.inccommand = "split"
+
+vim.opt.mouse = "a"
+
+-- netrw settings
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro" -- netrw line num
+
