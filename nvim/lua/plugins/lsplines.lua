b57f8d29582005ac5c1a28717db8b38d7a199812
c-santos <jcms121899@gmail.com>
2024-10-23 10:49:43 +0800

    nvim: turn off lsplines by default

--- nvim/lua/plugins/lsplines.lua
+++ nvim/lua/plugins/lsplines.lua
@@ -3,6 +3,10 @@ return {
     config = function()
         local lsp_lines = require("lsp_lines")
         lsp_lines.setup({})
+        -- turn off by default
+        vim.diagnostic.config({
+            virtual_lines = false
+        })
         vim.keymap.set("n", "<leader>l", lsp_lines.toggle)
     end
 }
