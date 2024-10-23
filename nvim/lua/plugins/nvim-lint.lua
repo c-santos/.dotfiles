dc780eac44d9014faa251f4f9b8e096aeb43b2e6
Carlo Santos <jcms121899@gmail.com>
2024-10-23 11:55:57 +0800

    nvim: formatting and linting (#1)
    * added conform.nvim for formatting
    
    * deleted unused plugins
    
    * added nvim-lint for linting

---
+++ nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,15 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescript = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+		}
+		vim.keymap.set("n", "<leader>lf", lint.try_lint, { desc = "[L]int [F]ile" })
+	end,
+}
