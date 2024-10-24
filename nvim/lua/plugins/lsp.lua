7990b7c1092877542e761870cc685915410f5831
c-santos <jcms121899@gmail.com>
2024-10-25 01:02:08 +0800

    nvim: use telescope for gd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -38,7 +38,7 @@ return {
             nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
             -- nmap("<leader>f", vim.lsp.buf.format, "[F]ormat")
             nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-            nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+            nmap("gd", require('telescope.builtin').lsp_definitions, "[G]oto [D]efinition")
             nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
             nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
             nmap("gtd", vim.lsp.buf.type_definition, "[G]oto [T]ype [D]efinition")
