5fff1534c9a86406a3c5220d54d9bb0e550608c6
c-santos <jcms121899@gmail.com>
2024-10-31 21:51:18 +0800

    nvim: added code action telescope integration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
         "rafamadriz/friendly-snippets",
         "L3MON4D3/LuaSnip",
         "j-hui/fidget.nvim",
-        "ray-x/lsp_signature.nvim"
+        "ray-x/lsp_signature.nvim",
     },
     config = function()
         -- Setup plugins without custom config
@@ -26,26 +26,49 @@ return {
         -- This function runs when an LSP attaches to the buffer.
         local on_attach = function(_, bufnr)
             -- Remaps
+
             local nmap = function(keys, func, desc)
                 if desc then
-                    desc = 'LSP: ' .. desc
+                    desc = "LSP: " .. desc
                 end
 
                 vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
             end
 
             nmap("K", vim.lsp.buf.hover, "Hover documentation.")
-            nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+            -- nmap("<leader>ca", function () vim.lsp.buf.code_action() end, "[C]ode [A]ction")
             -- nmap("<leader>f", vim.lsp.buf.format, "[F]ormat")
             nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-            nmap("gd", require('telescope.builtin').lsp_definitions, "[G]oto [D]efinition")
+            nmap(
+                "gd",
+                require("telescope.builtin").lsp_definitions,
+                "[G]oto [D]efinition"
+            )
             nmap("gb", "<c-t>", "[G]o [b]ack with tagstack")
             nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-            nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-            nmap("gtd", vim.lsp.buf.type_definition, "[G]oto [T]ype [D]efinition")
-            nmap("<leader>ws", require("telescope.builtin").lsp_workspace_symbols, "[G]oto [W]orkspace [Symbols]")
-            nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[G]oto [D]ocument [Symbols]")
-
+            nmap(
+                "gr",
+                require("telescope.builtin").lsp_references,
+                "[G]oto [R]eferences"
+            )
+            nmap(
+                "gtd",
+                vim.lsp.buf.type_definition,
+                "[G]oto [T]ype [D]efinition"
+            )
+            nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+            -- Symbols in current doc / buf
+            nmap(
+                "<leader>ds",
+                require("telescope.builtin").lsp_document_symbols,
+                "[G]oto [D]ocument [Symbols]"
+            )
+            -- Symbols in whole project
+            nmap(
+                "<leader>ws",
+                require("telescope.builtin").lsp_workspace_symbols,
+                "[G]oto [W]orkspace [Symbols]"
+            )
 
             -- Signature help
             local signature_help = {
@@ -56,8 +79,8 @@ return {
                 hint_prefix = {
                     above = "↙ ", -- when the hint is on the line above the current line
                     current = "← ", -- when the hint is on the same line
-                    below = "↖ " -- when the hint is on the line below the current line
-                }
+                    below = "↖ ", -- when the hint is on the line below the current line
+                },
             }
             require("lsp_signature").on_attach(signature_help, bufnr)
         end
@@ -76,35 +99,47 @@ return {
             ensure_installed = language_servers,
             handlers = {
                 function(server_name)
-                    require("lspconfig")[server_name].setup {
+                    require("lspconfig")[server_name].setup({
                         capabilities = capabilities,
                         on_attach = on_attach,
                         handlers = {
-                            ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-                        }
-                    }
+                            ["textDocument/hover"] = vim.lsp.with(
+                                vim.lsp.handlers.hover,
+                                { border = "rounded" }
+                            ),
+                        },
+                    })
                 end,
 
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
+                    lspconfig.lua_ls.setup({
                         capabilities = capabilities,
                         on_attach = on_attach,
                         handlers = {
-                            ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+                            ["textDocument/hover"] = vim.lsp.with(
+                                vim.lsp.handlers.hover,
+                                { border = "rounded" }
+                            ),
                         },
                         settings = {
                             Lua = {
                                 runtime = { version = "LuaJIT" },
                                 diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
+                                    globals = {
+                                        "bit",
+                                        "vim",
+                                        "it",
+                                        "describe",
+                                        "before_each",
+                                        "after_each",
+                                    },
+                                },
+                            },
+                        },
+                    })
                 end,
             },
-
         })
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -112,7 +147,7 @@ return {
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
                 end,
             },
             window = {
@@ -120,35 +155,35 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(cmp_select),
-                ['<C-f>'] = cmp.mapping.scroll_docs(cmp_select),
-                ['<C-Space>'] = cmp.mapping.complete({ select = true }),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<C-b>"] = cmp.mapping.scroll_docs(cmp_select),
+                ["<C-f>"] = cmp.mapping.scroll_docs(cmp_select),
+                ["<C-Space>"] = cmp.mapping.complete({ select = true }),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                { name = "nvim_lsp" },
+                { name = "luasnip" }, -- For luasnip users.
             }, {
-                { name = 'buffer' },
-            })
+                { name = "buffer" },
+            }),
         })
 
-        cmp.setup.cmdline({ '/', '?' }, {
+        cmp.setup.cmdline({ "/", "?" }, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
-                { name = 'buffer' }
-            }
+                { name = "buffer" },
+            },
         })
 
-        cmp.setup.cmdline(':', {
+        cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
-                { name = 'path' }
+                { name = "path" },
             }, {
-                { name = 'cmdline' }
+                { name = "cmdline" },
             }),
-            matching = { disallow_symbol_nonprefix_matching = false }
+            matching = { disallow_symbol_nonprefix_matching = false },
         })
 
         -- Diagnostics
@@ -165,7 +200,7 @@ return {
                 border = "rounded",
                 source = "always",
                 header = "",
-            }
+            },
         })
-    end
+    end,
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,77 +10,148 @@ table.insert(vimgrep_arguments, "--glob")
 table.insert(vimgrep_arguments, "!**/.git/*")
 
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
-    config = function()
-        local actions = require("telescope.actions")
-        require("telescope").setup({
-            defaults = {
-                vimgrep_arguments = vimgrep_arguments,
-                mappings = {
-                    i = {
-                        ["<esc>"] = actions.close
-                    }
-                },
-                layout_strategy = 'flex',
-                layout_config = {
-                    horizontal = {
-                        width = 0.9,
-                        height = 0.9,
-                        prompt_position = 'bottom'
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        event = "VimEnter",
+        dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
+        config = function()
+            local actions = require("telescope.actions")
+            require("telescope").setup({
+                defaults = {
+                    vimgrep_arguments = vimgrep_arguments,
+                    mappings = {
+                        i = {
+                            ["<esc>"] = actions.close,
+                        },
                     },
-                    vertical = {
-                        width = 0.9,
-                        height = 0.9,
-                        prompt_position = 'bottom'
+                    layout_strategy = "flex",
+                    layout_config = {
+                        horizontal = {
+                            width = 0.9,
+                            height = 0.9,
+                            prompt_position = "bottom",
+                        },
+                        vertical = {
+                            width = 0.9,
+                            height = 0.9,
+                            prompt_position = "bottom",
+                        },
                     },
-                }
-            },
-            pickers = {
-                find_files = {
-                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
                 },
-            },
-        })
+                pickers = {
+                    find_files = {
+                        -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                        find_command = {
+                            "rg",
+                            "--files",
+                            "--hidden",
+                            "--glob",
+                            "!**/.git/*",
+                        },
+                    },
+                },
+            })
 
-        local builtin = require("telescope.builtin")
+            local builtin = require("telescope.builtin")
 
-        -- Search all project files
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+            -- Search all project files
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 
-        -- Search string
-        vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+            -- Search string
+            vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 
-        -- Search string under cursor
-        vim.keymap.set("n", "<leader>pws", function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end, {})
+            -- Search string under cursor
+            vim.keymap.set("n", "<leader>pws", function()
+                local word = vim.fn.expand("<cword>")
+                builtin.grep_string({ search = word })
+            end, {})
 
-        -- Search string under cursor big
-        vim.keymap.set("n", "<leader>pWs", function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end, {})
+            -- Search string under cursor big
+            vim.keymap.set("n", "<leader>pWs", function()
+                local word = vim.fn.expand("<cWORD>")
+                builtin.grep_string({ search = word })
+            end, {})
 
-        -- Search buffers
-        vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
+            -- Search buffers
+            vim.keymap.set(
+                "n",
+                "<leader>pb",
+                builtin.buffers,
+                { desc = "[P]roject [b]uffers" }
+            )
 
-        -- Git commits
-        vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+            -- Git commits
+            vim.keymap.set(
+                "n",
+                "<leader>gc",
+                builtin.git_commits,
+                { desc = "[G]it [c]ommits" }
+            )
 
-        -- Git branches
-        vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+            -- Git branches
+            vim.keymap.set(
+                "n",
+                "<leader>gb",
+                builtin.git_branches,
+                { desc = "[G]it [b]ranches" }
+            )
 
-        -- Git commits of current buffer
-        vim.keymap.set("n", "<leader>bc", builtin.git_bcommits, {})
+            -- Git commits of current buffer
+            vim.keymap.set(
+                "n",
+                "<leader>bc",
+                builtin.git_bcommits,
+                { desc = "[B]uffer [c]ommits" }
+            )
 
-        -- List telescope builtin pickers
-        vim.keymap.set("n", "<leader>bi", builtin.builtin, {})
+            -- List telescope builtin pickers
+            vim.keymap.set(
+                "n",
+                "<leader>bi",
+                builtin.builtin,
+                { desc = "[B]uilt[i]n" }
+            )
 
-        -- Find in current buffer
-        vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find, {})
-    end
+            -- Find in current buffer
+            vim.keymap.set("n", "<C-f>", function()
+                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                builtin.current_buffer_fuzzy_find(
+                    require("telescope.themes").get_dropdown({
+                        winblend = 10,
+                        previewer = false,
+                        prompt_title = "Buffer search",
+                    })
+                )
+            end, { desc = "[B]uffer [s]earch" })
+
+            -- Search in open files
+            vim.keymap.set("n", "<leader>os", function()
+                builtin.live_grep({
+                    grep_open_files = true,
+                    prompt_title = "Search in open files",
+                })
+            end, { desc = "[O]pen files [s]earch" })
+
+            vim.keymap.set("n", "<leader>ca", function()
+                vim.lsp.buf.code_action()
+            end)
+        end,
+    },
+    {
+        -- Makes vim.ui.select use telescope
+        -- Allows core neovim stuff to use telescope
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown(),
+                    },
+                },
+            })
+
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
