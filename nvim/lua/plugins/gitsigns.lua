32b93692c43c996f45ef7477da896fafe4adb203
c-santos <jcms121899@gmail.com>
2024-11-03 16:19:17 +0800

    nvim: update gitsigns config

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,14 +1,31 @@
 return {
     "lewis6991/gitsigns.nvim",
-    config = function ()
-        require('gitsigns').setup({
-            current_line_blame = true
+    config = function()
+        local gitsigns = require('gitsigns')
+        gitsigns.setup({
+            current_line_blame = true,
+            on_attach = function(bufnr)
+                local map = function (mode, key, cmd, opts)
+                    opts = { buffer = bufnr }
+                    vim.keymap.set(mode, key, cmd, opts)
+                end
+
+                map("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+                map("n", "<leader>gu", ":Gitsigns reset_hunk<CR>")
+
+                map("v", "<leader>sh", function()
+                    gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') }
+                end)
+                map("n", "<leader>sh", ":Gitsigns stage_hunk<CR>")
+
+                map("n", "<leader>su", ":Gitsigns undo_stage_hunk<CR>")
+                map("v", "<leader>su", function()
+                    gitsigns.undo_stage_hunk { vim.fn.line('.'), vim.fn.line('v') }
+                end)
+
+                map("n", "]c", ":Gitsigns next_hunk<CR>")
+                map("n", "[c", ":Gitsigns prev_hunk<CR>")
+            end
         })
-        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
-        vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>")
-        vim.keymap.set("n", "<leader>sh", ":Gitsigns stage_hunk<CR>")
-        vim.keymap.set("n", "<leader>su", ":Gitsigns undo_stage_hunk<CR>")
-        vim.keymap.set("n", "]c", ":Gitsigns next_hunk<CR>")
-        vim.keymap.set("n", "[c", ":Gitsigns prev_hunk<CR>")
     end
 }
