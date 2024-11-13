7156447ea379ee3e3d21ce8b4a5c07cbb139785e
c-santos <jcms121899@gmail.com>
2024-11-13 10:58:49 +0800

    wezterm: updated line height

--- wezterm/fonts.lua
+++ wezterm/fonts.lua
@@ -4,16 +4,16 @@ local M = {}
 ---@param config table
 M.setup = function(config)
     -- Global options
-    config.line_height = 1.25
+    config.line_height = 1.24
     config.font_size = 14
     config.adjust_window_size_when_changing_font_size = false
 
     local current_os = GetOS()
     if current_os == "unix" then
         config.font = wz.font_with_fallback({
+            { family = "CodeNewRoman Nerd Font Mono", weight = 400 },
             { family = "FiraCode Nerd Font", weight = 400 },
             { family = "BlexMono Nerd Font Mono", weight = 400 },
-            { family = "CodeNewRoman Nerd Font Mono", weight = 400 },
             { family = "JetBrainsMono Nerd Font", weight = 400 },
             { family = "BigBlueTerm437 Nerd Font Mono", weight = 400 },
             { family = "Hack Nerd Font", weight = 400 },
