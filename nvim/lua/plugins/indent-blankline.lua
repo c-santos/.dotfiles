6efa842c2e836c2bb429b94e1b2d91199bc6376a
c-santos <jcms121899@gmail.com>
2024-11-18 18:31:20 +0800

    wezterm theme updates

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,6 +2,9 @@
 return {
     -- "lukas-reineke/indent-blankline.nvim",
     -- main = "ibl",
+    -- ---@module "ibl"
+    -- ---@type ibl.config
+    -- opts = {},
     -- config = function()
     --     local highlight = {
     --         "RainbowOrange",
@@ -11,7 +14,7 @@ return {
     --         "RainbowViolet",
     --     }
     --
-    --     local hooks = require "ibl.hooks"
+    --     local hooks = require("ibl.hooks")
     --     -- create the highlight groups in the highlight setup hook, so they are reset
     --     -- every time the colorscheme changes
     --     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
@@ -21,7 +24,7 @@ return {
     --         vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
     --         vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
     --     end)
-    --     require('ibl').setup({
+    --     require("ibl").setup({
     --         indent = {
     --             char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
     --         },
@@ -29,10 +32,9 @@ return {
     --             remove_blankline_trail = false,
     --         },
     --         scope = {
-    --             enabled = true,
-    --             highlight = highlight
-    --         }
+    --             enabled = false,
+    --             -- highlight = highlight,
+    --         },
     --     })
     -- end,
-    -- opts = {},
 }
