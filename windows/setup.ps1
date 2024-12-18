d7feb81c43855407b28a17fd5c589c57cb3d70d7
c-santos <jcms121899@gmail.com>
2024-12-18 14:03:44 +0800

    added starship install and symlink

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,10 +1,13 @@
 $PWSH_FILENAME="Microsoft.Powershell_Profile_Custom.ps1"
 $DOTFILES_LOCATION="E:\Files\dev\.dotfiles"
 
-# install neovim dependencies
-winget install starship
+# install dependencies
+winget install starship # shell
 winget install BurntSushi.ripgrep.MSVC
 winget install junegunn.fzf
+
+# symlink starship
+New-Item -ItemType SymbolicLink -Path "$HOME\.config\starship.toml" -Target "$DOTFILES_LOCATION\windows\starship.toml"
 
 # symlink nvim
 New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim" -Target "$DOTFILES_LOCATION\nvim"
