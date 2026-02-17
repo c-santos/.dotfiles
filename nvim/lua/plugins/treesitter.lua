431e35edf4e14241b7dae0ade084c6383e94539c
c-santos <jcms121899@gmail.com>
2026-02-17 22:23:37 +0800

    nvim: use master branch in treesitter after breaking changes

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "master",
         build = ":TSUpdate",
         event = {"BufReadPre", "BufNewFile"},
         config = function ()
