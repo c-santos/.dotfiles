9dab3777a8372c6212cce0b6d29916112cf95c6f
c-santos <jcms121899@gmail.com>
2026-02-28 14:25:42 +0800

    nvim: cleanup

--- nvim/lua/plugins/autopair.lua
+++ nvim/lua/plugins/autopair.lua
@@ -1,12 +1,4 @@
 return {
-    -- {
-    --     'altermo/ultimate-autopair.nvim',
-    --     event={'InsertEnter'},
-    --     branch='v0.6', --recommended as each new version will have breaking changes
-    --     opts={
-    --         --Config goes here
-    --     },
-    -- },
     {
         'windwp/nvim-autopairs',
         event = 'InsertEnter',
--- nvim/theme-default.lua
+++
@@ -1,8 +0,0 @@
--- Default theme
-local theme = {
-    dark = "catppuccin-mocha",
-    light = "catppucin-frappe",
-    auto_light_mode = true
-}
-
-return theme
